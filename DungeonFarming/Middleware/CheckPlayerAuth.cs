20d0dea76606899bd578d4c5ca58987e0a5a79f9
SeongJae Han <anis15740@gmail.com>
2023-05-01 22:10:07 +0900

    .

---
+++ DungeonFarming/Middleware/CheckPlayerAuth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DungeonFarming.DBTableFormat;
+using Microsoft.AspNetCore.Http;
+
+namespace DungeonFarming.Middleware;
+
+public class CheckPlayerAuth
+{
+    private readonly Services.IRedisDb _RedisDb;
+    private readonly RequestDelegate _next;
+
+    public CheckPlayerAuth(RequestDelegate next, Services.IRedisDb redisDb)
+    {
+        _RedisDb = redisDb;
+        _next = next;
+    }
+}
